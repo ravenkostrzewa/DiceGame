a67b7db732b374af874fd28ace8f70684acb3d69
ravenkostrzewa <ravekostrzewa@gmail.com>
2017-08-22 16:09:21 -0500

    first commit

---
+++ DiceGame.js
@@ -0,0 +1,51 @@
+function rollTheFour-SidedDie
+
+
+    var x = Math.floor(Math.random() * ((4-1)+1) + 1);
+   
+		if x = 1
+			console.log("Roll the eight-sided die.");
+		if x = 2
+			console.log("Roll the ten-sided die.");
+		if x = 3
+			console.log("Roll the twelve-sided die.");
+		if x = 4
+			console.log("Roll the twenty-sided die.");
+		
+		
+		
+		Prompt: Choose an attack: 1) pursue, 2) search out, or 3) shoot.
+
+function rollTheSix-SidedDie () {
+for (var x = Math.floor(Math.random() * ((6-1)+1) + 1));
+		if (x === 1 || x === 2) {
+			console.log("Run!");	
+		else if (x === 3 || x === 4) {
+			console.log("Hide!")
+		else if (x === 5 || x === 6) {
+			console.log("Shield Yourself!")
+			}
+		}
+	}
+}
+
+
+	if () //attack is defended 
+		console.log("The defendant prevails!");	
+
+	else if () //attack is not defended
+		console.log("A merciless attack! The defendant cannot hold its ground. Retreat 8 paces.")
+		
+	else if () //attack is not defended
+		console.log("A merciless attack! The defendant cannot hold its ground. Retreat 10 paces.")
+	
+	else if () //attack is not defended
+		console.log("A merciless attack! The defendant cannot hold its ground. Retreat 12 paces.")
+	
+	else if () //attack is not defended
+		console.log("A merciless attack! The defendant cannot hold its ground. Retreat 20 paces.")
+
+		
+		
+		
+// when one player is forced back 50+ paces, the other player wins
