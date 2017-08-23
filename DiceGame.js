830c17f436ebf74c0368aec4f79146d0d04e651e
ravenkostrzewa <ravekostrzewa@gmail.com>
2017-08-23 12:00:16 -0500

    outlined project

--- DiceGame.js
+++ DiceGame.js
@@ -1,70 +1,69 @@
-
-
 function rollFourSidedDie(){
-	var x = Math.floor((Math.random() * 4) + 1);
+	var z = Math.floor((Math.random() * 4) + 1);
    
-		if (x === 1) {
-			console.log("Roll the eightSided die.");
+		if (z === 1) {
+			console.log("Roll the Eight-Sided Die.");
 			rollEightSidedDie();
 		}
-		else if (x === 2) {
-			console.log("Roll the tenSided die.");
+		else if (z === 2) {
+			console.log("Roll the Ten-Sided Die.");
 			rollTenSidedDie();	
 			}
-		else if (x === 3) {
-			console.log("Roll the twelveSided die.");
+		else if (z === 3) {
+			console.log("Roll the Twelve-Sided Die.");
 			rollTwelveSidedDie();
 		}
-		else if (x === 4) {
-			console.log("Roll the twentySided die.");
+		else if (z === 4) {
+			console.log("Roll the Twenty-Sided Die.");
 			rollTwentySidedDie();
 		}
 	}	
-
-function rollEightSidedDie(playerLocation) {
+		
+	
+function rollEightSidedDie(playerLocation) {	
+	var userWord = prompt("Attacker, how would you like to combat your opponent? Enter the corresponding number to choose your strategy: 1) trick, 2) trap, or 3) sword fight.");
 	var x = Math.floor((Math.random() * 8) + 1);
-	playerLocation += x;
-	console.log("You moved forward " + x + " spaces.";
-	rollTenSidedDie(playerLocation)
+	playerLocation += y;
+	console.log("You advanced " + x + " spaces.";
+	rollEightSidedDie(playerLocation)
 }
 
 		
 function rollTenSidedDie(playerLcation) {
-var x = Math.floor((Math.random() * 10) + 1);
-
-
-
-function rollTwelveSidedDie() {
-var x = Math.floor((Math.random() * 12) + 1);
+	var userWord = prompt("Attacker, how would you like to combat your opponent? Enter the corresponding number to choose your strategy: 1) trick, 2) trap, or 3) sword fight.");
+	var x = Math.floor((Math.random() * 12) + 1);
+	playerLocation += x;
+	console.log("You advanced " + x + " spaces.";
+	rollTwelveSidedDie(playerLocation)
+}
 
 
 
 function rollTwentySidedDie() {
-var x = Math.floor((Math.random() * 20) + 1);		
+	var v = prompt("Attacker, how would you like to combat your opponent? Enter the corresponding number to choose your strategy: 1) trick, 2) trap, or 3) sword fight.");
+	var x = Math.floor((Math.random() * 20) + 1);		
+	playerLocation += x;
+	console.log("You advanced " + x + " spaces.";
+	rollTwentySidedDie(playerLocation)
+}
 		
 		
 		
-		
-		
-	
-		
-		
-var userWord = prompt("Attacker, choose an attack: 1) pursue, 2) search out, or 3) shoot.");
-
-// roll die		
- 
-
-		//defender:
 function rollSix-SidedDie() {
-	var y = sixSidedDie
+	var w = sixSidedDie
 	
 for sixSidedDie = 1 + randomsource.nextInt(6);
-		if (y === 1 || y === 2) {
-			console.log("Run!");	
-		else if (y === 3 || y === 4) {
-			console.log("Hide!")
-		else if (y === 5 || y === 6) {
-			console.log("Shield Yourself!")
+		if (w === 1 || w === 2) {
+			console.log("Be clever!");
+			battles();
+			}
+		else if (w === 3 || w === 4) {
+			console.log("Be illusive!")
+			battles();
+		}
+		else if (w === 5 || w === 6) {
+			console.log("Defend Yourself!")
+			battles();
 			}
 		}
 	}
@@ -72,43 +71,48 @@ for sixSidedDie = 1 + randomsource.nextInt(6);
 	
 function battles() {
 	
-	if attacker chose 1 and (y === 1 || y === 2) {
-		console.log("What a chase! The defendant prevails!");	
-
-	else if attacker chose 1 and (y === 3 || y === 4) {
-		console.log("Gothca! Retreat -- paces.");
-		
-	else if attacker chose 1 and (y === 5 || y === 6) {
-		console.log("A merciless attack! Retreat -- paces.");
-	
-	else if attacker chose 2 and (y === 3 || y === 4) {
-		console.log("Nice hiding spot! The defendant prevails!");	
-
-	else if attacker chose 2 and (y === 1 || y === 2) {
-		console.log("A merciless attack! The defendant cannot hold its ground. Retreat -- paces.");
-		
-	else if attacker chose 2 and (y === 5 || y === 6) {
-		console.log("A successful attack! Retreat -- paces.");
-
-	else if attacker chose 3 and (y === 5 || y === 6) {
-		console.log("You saw that coming! The defendant prevails!");	
-
-	else if attacker chose 3 and (y !== 1 || y !== 2) {
-		console.log("You cannot outrun from that shot! Retreat -- paces.");
-		
-	else if attacker chose 3 and (y !== 3 || y !== 4) {
-		console.log("You cannot hide from that shot! Retreat -- paces.");		
+	if (v === 1) && (w === 1 || w === 2) {
+		console.log("The trick failed! Great defense! Advance " x " paces.");	//Defense advances
+		}
+	}
+	else if (v === 1) && (w === 3 || w === 4) {
+		console.log("Gotcha! Nice trick attacker! Advance " x " paces.");
+		//attacker (x) {
+		}
+	}
+	else if (v === 1) and (w === 5 || w === 6) {
+		console.log("Gotcha! Nice trick attacker! Advance " x " paces.");
+		//attacker (x) {
+		}
+	}
+	else if (v === 2) and (w === 3 || w === 4) {
+		console.log("Nice hiding spot! Great defense! Advance " x " paces.");	//Defense advances
+		}
+	}
+	else if (v === 2) and (w === 1 || w === 2) {
+		console.log("Wow! They fell for that?! Nice trap attacker! Advance " x " paces.");
+		//attacker (x) {
+		}
+	}
+	else if (v === 2) and (w === 5 || w === 6) {
+		console.log("Wow! They fell for that?! Nice trap attacker! Advance " x " paces.");
+		//attacker (x) {
+		}
+	}
+	else if (v === 3) and (w === 5 || w === 6) {
+		console.log("Relentless resilience! Great defense! Advance " x " paces.");	 //Defense advances
+		}
+	}
+	else if (v === 3) and (w === 1 || w === 2) {
+		console.log("On guard! Smooth sword skills attacker! Advance " x " paces.");
+		//attacker (x) {
+		}
+	}
+	else if (v === 3) and (w === 3 || w === 4) {
+		console.log("On guard! Smooth sword skills attacker! Advance " x " paces.");
+		//attacker (x) {
 		}	
-	}	
-}
+	}
 	
 	
 	
-	
-var player1Location = 1;
-var player2Location = 1;
-rollFourSidedDie(player1Location);	
-rollEightSidedDie(player2Location);
-		
-		
-// when one player is forced back 50+ paces, the other player wins
